b191398a549461ddfa6c05251cbc72a964f36847
brandonocasey <brandonocasey@gmail.com>
2018-04-05 11:01:27 -0400

    fix source inconsistencies

--- src/js/player.js
+++ src/js/player.js
@@ -2611,8 +2611,6 @@ class Player extends Component {
     }
 
     if (!titleCaseEquals(sourceTech.tech, this.techName_)) {
-      this.changingSrc_ = true;
-
       // load this technology with the chosen source
       this.loadTech_(sourceTech.tech, sourceTech.source);
       return false;
